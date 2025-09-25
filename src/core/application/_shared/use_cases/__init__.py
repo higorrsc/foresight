430bbd914e8c591bb615ed93acde5696ca93d94f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-25 19:48:35 -0300

    feat: ✨ add init file to shared use cases

---
+++ src/core/application/_shared/use_cases/__init__.py
@@ -0,0 +1,9 @@
+from .generic_delete import GenericDeleteUseCase, InputDeleteRequestDTO
+from .generic_list import GenericListUseCase, OutputGenericListDTO
+
+__all__ = [
+    "GenericDeleteUseCase",
+    "InputDeleteRequestDTO",
+    "GenericListUseCase",
+    "OutputGenericListDTO",
+]
