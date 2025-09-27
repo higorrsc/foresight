75b0a4d200d9caa770da568b71316e2fd5b146a4
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-26 21:55:00 -0300

    feat: ✨ add uvicorn server to FastAPI implementation

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import uvicorn
+
+
 def main():
-    print("Hello from foresight!")
+    """
+    Entry point to run the FastAPI application using Uvicorn.
+    """
+    uvicorn.run(
+        "src.api.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
 
 
 if __name__ == "__main__":
