cfa47c2aae8b0b8750f3ab316596180a2ef500f7
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-27 15:07:03 -0300

    fix: :bug: correct id field type at area model

--- src/core/infrastructure/models/area_model.py
+++ src/core/infrastructure/models/area_model.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import uuid4
 
-from sqlalchemy import Column, DateTime, String
+from sqlalchemy import UUID, Column, DateTime, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -15,9 +15,9 @@ class AreaModel(Base):
     __tablename__ = "areas"
 
     id = Column(
-        String(36),
+        UUID(as_uuid=True),
         primary_key=True,
-        default=lambda: str(uuid4()),
+        default=uuid4,
     )
     description = Column(
         String(100),
