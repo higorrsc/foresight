f58d61adefd22225a0ddb7ecceb1b56048ca7382
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-27 12:11:03 -0300

    feat: ✨ add GetAreaById use case with tests

--- src/core/application/use_cases/area/__init__.py
+++ src/core/application/use_cases/area/__init__.py
@@ -1,6 +1,7 @@
 from .exceptions import AreaNotFoundError, InvalidAreaError
 from .create_area import CreateAreaInputDTO, CreateAreaOutputDTO, CreateAreaUseCase
 from .delete_area import DeleteAreaUseCase
+from .get_area_by_id import GetAreaByIdUseCase
 from .list_area import ListAreaUseCase
 from .update_area import UpdateAreaInputDTO, UpdateAreaOutputDTO, UpdateAreaUseCase
 
@@ -15,4 +16,5 @@ __all__ = [
     "UpdateAreaInputDTO",
     "UpdateAreaOutputDTO",
     "InvalidAreaError",
+    "GetAreaByIdUseCase",
 ]
---
+++ src/core/application/use_cases/area/get_area_by_id.py
@@ -0,0 +1,21 @@
+from src.core.application._shared.use_cases import GenericGetByIdUseCase
+from src.core.application.use_cases.area.exceptions import AreaNotFoundError
+from src.core.domain._shared.repository import AbstractRepository
+from src.core.domain.entities import Area
+
+
+class GetAreaByIdUseCase(GenericGetByIdUseCase[Area]):
+    """
+    Use case for getting an area by its ID.
+    """
+
+    def __init__(self, repository: AbstractRepository[Area]) -> None:
+        """
+        Initialize the get by id use case.
+        """
+
+        super().__init__(
+            repository,
+            AreaNotFoundError,
+            "Area with given ID not found.",
+        )
