ba1c957a25b77d281f27116ceb2fecdf3f401a45
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-27 18:37:06 -0300

    feat: ✨ add user mapper

--- src/core/infrastructure/mappers/__init__.py
+++ src/core/infrastructure/mappers/__init__.py
@@ -1,5 +1,7 @@
 from .area_mapper import AreaMapper
+from .user_mapper import UserMapper
 
 __all__ = [
     "AreaMapper",
+    "UserMapper",
 ]
---
+++ src/core/infrastructure/mappers/user_mapper.py
@@ -0,0 +1,36 @@
+from src.core.domain.entities import User
+from src.core.infrastructure.models import UserModel
+
+
+class UserMapper:
+    """
+    Mapper class to convert between User entity and UserModel.
+    """
+
+    @staticmethod
+    def to_model(entity: User) -> "UserModel":
+        """
+        Converts a User entity to a UserModel instance.
+        """
+
+        return UserModel(
+            id=entity.id,
+            username=entity.username,
+            password=entity.hashed_password,
+            created_at=entity.created_at,
+            updated_at=entity.updated_at,
+        )
+
+    @staticmethod
+    def to_entity(model: "UserModel") -> User:
+        """
+        Converts a UserModel instance to a User entity.
+        """
+
+        return User(
+            id=model.id,  # type: ignore
+            username=model.username,  # type: ignore
+            hashed_password=model.password,  # type: ignore
+            created_at=model.created_at,  # type: ignore
+            updated_at=model.updated_at,  # type: ignore
+        )
