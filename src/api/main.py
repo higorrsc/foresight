5c889a6bf60106b49d48a69bfb95f6d56c5100db
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-27 15:05:37 -0300

    feat: ✨ start database when start FastAPI

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
 from src.api.routers import area_router
+from src.core.infrastructure.config.database import Base, engine
+from src.core.infrastructure.models import AreaModel
+
+Base.metadata.create_all(bind=engine)
+AreaModel.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="Foresight API",
@@ -13,4 +19,8 @@ app.include_router(area_router.router)
 
 @app.get("/")
 def read_root():
+    """
+    Root of the API.
+    """
+
     return {"message": "Bem-vindo à Foresight API!"}
