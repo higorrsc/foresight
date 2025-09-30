fcf8b732533a4e05bf3e51e2939f612ea4aae9d4
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-30 19:34:27 -0300

    feat: ✨ add ListRoleUseCase with tests

--- src/core/application/use_cases/role/__init__.py
+++ src/core/application/use_cases/role/__init__.py
@@ -2,13 +2,15 @@ from .exceptions import InvalidRoleError, RoleNotFoundError
 from .create_role import CreateRoleInputDTO, CreateRoleOutputDTO, CreateRoleUseCase
 from .delete_role import DeleteRoleUseCase
 from .get_role_by_id import GetRoleByIdUseCase
+from .list_role import ListRoleUseCase
 
 __all__ = [
-    "CreateRoleUseCase",
     "CreateRoleInputDTO",
     "CreateRoleOutputDTO",
-    "RoleNotFoundError",
-    "InvalidRoleError",
+    "CreateRoleUseCase",
     "DeleteRoleUseCase",
     "GetRoleByIdUseCase",
+    "InvalidRoleError",
+    "ListRoleUseCase",
+    "RoleNotFoundError",
 ]
---
+++ src/core/tests/application/use_cases/role/test_list_role.py
@@ -0,0 +1,49 @@
+import pytest
+
+from src.core.application._shared.use_cases.generic_list import ListRequestInputDTO
+from src.core.application.use_cases.role.list_role import ListRoleUseCase
+from src.core.domain.entities import Role
+from src.core.infrastructure.repositories._shared import InMemoryRepository
+
+
+class TestListRoleUseCase:
+    """
+    Test suite for the ListRoleUseCase.
+    """
+
+    @pytest.fixture
+    def role_in_memory_repository(self):
+        """
+        Fixture that represents an in-memory repository for testing purposes.
+        """
+
+        return InMemoryRepository[Role]()
+
+    def test_list_role(self, role_in_memory_repository):
+        """
+        Test list role.
+        """
+
+        repo = role_in_memory_repository
+
+        new_role = Role(name="Test Role", description="Test Description")
+        repo.save(new_role)
+
+        use_case = ListRoleUseCase(repository=repo)
+
+        output = use_case.execute(ListRequestInputDTO())
+
+        assert output is not None
+        assert len(output.data) == 1
+        assert output.data[0].id == new_role.id
+
+    def test_list_role_with_empty_list(self, role_in_memory_repository):
+        """
+        Test list role with empty list.
+        """
+
+        repo = role_in_memory_repository
+        use_case = ListRoleUseCase(repository=repo)
+        output = use_case.execute(ListRequestInputDTO())
+
+        assert len(output.data) == 0
