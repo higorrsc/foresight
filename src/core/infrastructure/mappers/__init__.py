c75c239d5f9bf21612baa5ab089c221bad219740
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-09-30 19:00:13 -0300

    feat: ✨ add role mapper

--- src/core/infrastructure/mappers/__init__.py
+++ src/core/infrastructure/mappers/__init__.py
@@ -1,7 +1,9 @@
 from .area_mapper import AreaMapper
+from .role_mapper import RoleMapper
 from .user_mapper import UserMapper
 
 __all__ = [
     "AreaMapper",
     "UserMapper",
+    "RoleMapper",
 ]
