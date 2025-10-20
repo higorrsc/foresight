18007a2d8b8b8660a3082b20e873ac15c369054f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-10-20 19:07:01 -0300

    feat: :sparkles: add base class to represent a entity that has only description field

--- src/core/domain/_shared/__init__.py
+++ src/core/domain/_shared/__init__.py
@@ -1,15 +1,17 @@
 from .notification import Notification
 from .exceptions import EntityNotFoundException, EntityValidationError
 from .entity import AbstractEntity
+from .described_entity import DescribedEntity
 from .repository import AbstractRepository, PaginatedResult
 from .value_object import AbstractValueObject
 
 __all__ = [
     "AbstractEntity",
-    "AbstractValueObject",
-    "Notification",
     "AbstractRepository",
-    "EntityValidationError",
+    "AbstractValueObject",
+    "DescribedEntity",
     "EntityNotFoundException",
+    "EntityValidationError",
+    "Notification",
     "PaginatedResult",
 ]
---
+++ src/core/domain/_shared/described_entity.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass, field
+
+from src.core.domain._shared import AbstractEntity, EntityValidationError
+
+
+@dataclass(kw_only=True, eq=False)
+class DescribedEntity(AbstractEntity):
+    """
+    Base class for entities with only description field and default validations
+    """
+
+    description: str
+
+    _max_description_length: int = field(default=100, init=False, repr=False)
+
+    def update_description(self, new_description: str) -> None:
+        """
+        Update the description of the entity.
+
+        Args:
+            new_description (str): The new description for the entity.
+        """
+
+        self.description = new_description
+        self._validate()
+
+    def _validate(self) -> None:
+        """
+        Validate the entity.
+        """
+
+        if not self.description or not self.description.strip():
+            self.notification.add_error("Description must be a non-empty string.")
+
+        if len(self.description) > self._max_description_length:
+            self.notification.add_error(
+                f"Description must be at most {self._max_description_length} characters long."
+            )
+
+        if self.notification.has_errors:
+            raise EntityValidationError(self.notification.messages)
