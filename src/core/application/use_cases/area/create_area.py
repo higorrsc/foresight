5179181aa3a2085e558d785ce7795e93505746fe
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-10-20 19:39:35 -0300

    refactor: :recycle: inherit from generic create described entity use case

--- src/core/application/use_cases/area/create_area.py
+++ src/core/application/use_cases/area/create_area.py
@@ -1,31 +1,10 @@
-from dataclasses import dataclass, field
-from uuid import UUID, uuid4
-
+from src.core.application._shared.use_cases import CreateDescribedEntityUseCase
 from src.core.application.use_cases.area import InvalidAreaError
-from src.core.domain._shared import AbstractRepository, EntityValidationError
+from src.core.domain._shared import AbstractRepository
 from src.core.domain.entities import Area
 
 
-@dataclass
-class CreateAreaInputDTO:
-    """
-    Data Transfer Object for input data when creating a new area.
-    """
-
-    description: str
-    id: UUID = field(default_factory=uuid4)
-
-
-@dataclass
-class CreateAreaOutputDTO:
-    """
-    Data Transfer Object for output data when creating a new area.
-    """
-
-    id: UUID
-
-
-class CreateAreaUseCase:
+class CreateAreaUseCase(CreateDescribedEntityUseCase[Area]):
     """
     Create a new area.
     """
@@ -35,20 +14,8 @@ class CreateAreaUseCase:
         Initialize the CreateAreaUseCase.
         """
 
-        self._repository = repository
-
-    def execute(self, input_dto: CreateAreaInputDTO) -> CreateAreaOutputDTO:
-        """
-        Execute the use case to create a new area.
-        """
-
-        try:
-            area = Area(
-                id=input_dto.id,
-                description=input_dto.description,
-            )
-        except EntityValidationError as e:
-            raise InvalidAreaError(f"Invalid input data: {e}")
-
-        self._repository.save(area)
-        return CreateAreaOutputDTO(id=area.id)
+        super().__init__(
+            repository,
+            Area,
+            InvalidAreaError,
+        )
