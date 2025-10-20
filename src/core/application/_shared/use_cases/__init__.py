104213936f72c6855589087f54c8baee24ec73be
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-10-20 19:38:48 -0300

    feat: :sparkles: add generic create and update described entity use cases

--- src/core/application/_shared/use_cases/__init__.py
+++ src/core/application/_shared/use_cases/__init__.py
@@ -1,13 +1,29 @@
+from .generic_create_described import (
+    CreateDescribedEntityInputDTO,
+    CreateDescribedEntityOutputDTO,
+    CreateDescribedEntityUseCase,
+)
 from .generic_delete import DeleteRequestInputDTO, GenericDeleteUseCase
 from .generic_get_by_id import GenericGetByIdUseCase, GetByIdRequestInputDTO
 from .generic_list import GenericListUseCase, ListRequestInputDTO, ListResponseOutputDTO
+from .generic_update_described import (
+    UpdateDescribedEntityInputDTO,
+    UpdateDescribedEntityOutputDTO,
+    UpdateDescribedEntityUseCase,
+)
 
 __all__ = [
-    "GenericDeleteUseCase",
+    "CreateDescribedEntityInputDTO",
+    "CreateDescribedEntityOutputDTO",
+    "CreateDescribedEntityUseCase",
     "DeleteRequestInputDTO",
-    "GenericListUseCase",
-    "ListResponseOutputDTO",
+    "GenericDeleteUseCase",
     "GenericGetByIdUseCase",
+    "GenericListUseCase",
     "GetByIdRequestInputDTO",
     "ListRequestInputDTO",
+    "ListResponseOutputDTO",
+    "UpdateDescribedEntityInputDTO",
+    "UpdateDescribedEntityOutputDTO",
+    "UpdateDescribedEntityUseCase",
 ]
