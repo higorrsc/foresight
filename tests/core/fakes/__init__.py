19d36d7898b6d62129acc1f48b7752b93448598b
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-10-20 18:41:41 -0300

    refactor: :truck: rename InMemoryRepository at fakes

--- tests/core/fakes/__init__.py
+++ tests/core/fakes/__init__.py
@@ -1,5 +1,5 @@
 from .dummy_entity import DummyEntity
-from .in_memory_user_repository import RoleInMemoryRepository, UserInMemoryRepository
+from .in_memory_repository import RoleInMemoryRepository, UserInMemoryRepository
 
 __all__ = [
     "DummyEntity",
