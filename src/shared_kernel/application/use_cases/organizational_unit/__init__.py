49578aa2592a7288b07c44df81dd793bb2ab6238
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-10-29 21:09:40 -0300

    feat: :sparkles: add organizational unit exceptions

---
+++ src/shared_kernel/application/use_cases/organizational_unit/__init__.py
@@ -0,0 +1,6 @@
+from .exceptions import InvalidOrganizationalUnitError, OrganizationalUnitNotFoundError
+
+__all__ = [
+    "OrganizationalUnitNotFoundError",
+    "InvalidOrganizationalUnitError",
+]
---
+++ src/shared_kernel/application/use_cases/organizational_unit/exceptions.py
@@ -0,0 +1,10 @@
+class OrganizationalUnitNotFoundError(Exception):
+    """
+    Exception raised when an Organizational Unit is not found in the repository.
+    """
+
+
+class InvalidOrganizationalUnitError(Exception):
+    """
+    Exception raised when an Organizational Unit is not valid.
+    """
