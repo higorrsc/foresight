5fcd01295c254945cf8a02e40f2d37a0ccf243ae
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-10-31 19:50:38 -0300

    refactor: :recycle: create a specific use case to create organizational unit

--- src/shared_kernel/application/use_cases/organizational_unit/commands/create_organizational_unit.py
+++ src/shared_kernel/application/use_cases/organizational_unit/commands/create_organizational_unit.py
@@ -1,25 +1,63 @@
-from src.shared_kernel.application._shared.use_cases.commands import (
-    CreateDescribedEntityUseCase,
-)
+from dataclasses import dataclass, field
+from typing import Optional
+from uuid import UUID
+
 from src.shared_kernel.application.use_cases.organizational_unit import (
     InvalidOrganizationalUnitError,
 )
-from src.shared_kernel.domain._shared import AbstractRepository
+from src.shared_kernel.domain._shared import EntityValidationError
 from src.shared_kernel.domain.entities import OrganizationalUnit
+from src.shared_kernel.domain.repositories import IOrganizationalUnitRepository
 
 
-class CreateOrganizationalUnitUseCase(CreateDescribedEntityUseCase[OrganizationalUnit]):
+@dataclass(frozen=True)
+class CreateOrganizationalUnitInputDTO:
+    """
+    Data Transfer Object for input data when creating a new Organizational Unit.
+    """
+
+    description: str
+    code: str
+    parent_id: Optional[UUID] = field(default=None)
+
+
+@dataclass(frozen=True)
+class CreateOrganizationalUnitOutputDTO:
+    """
+    Data Transfer Object for output data when creating a new Organizational Unit.
+    """
+
+    id: UUID
+
+
+class CreateOrganizationalUnitUseCase:
     """
     Create a new organizationalUnit.
     """
 
-    def __init__(self, repository: AbstractRepository[OrganizationalUnit]) -> None:
+    def __init__(self, repository: IOrganizationalUnitRepository) -> None:
         """
         Initialize the CreateOrganizationalUnitUseCase.
         """
 
-        super().__init__(
-            repository,
-            OrganizationalUnit,
-            InvalidOrganizationalUnitError,
-        )
+        self._repository = repository
+
+    def execute(
+        self,
+        input_dto: CreateOrganizationalUnitInputDTO,
+    ) -> CreateOrganizationalUnitOutputDTO:
+        """
+        Execute the use case to create a new Organizational Unit.
+        """
+
+        try:
+            entity = OrganizationalUnit(
+                description=input_dto.description,
+                code=input_dto.code,
+                parent_id=input_dto.parent_id,
+            )
+        except EntityValidationError as e:
+            raise InvalidOrganizationalUnitError(f"Invalid input data: {e}") from e
+
+        self._repository.save(entity)
+        return CreateOrganizationalUnitOutputDTO(id=entity.id)
