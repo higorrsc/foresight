5c5a87532f1b6d04335bed66726b6ee1a022d8ec
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-10-31 19:52:08 -0300

    refactor: ♻ create a specific use case to get organizational unit by parent id

--- src/shared_kernel/application/use_cases/organizational_unit/queries/get_organizational_unit_by_parent_id.py
+++ src/shared_kernel/application/use_cases/organizational_unit/queries/get_organizational_unit_by_parent_id.py
@@ -1,1 +1,62 @@
-class GetOrganizationalUnitByParentIdUseCase: ...
+from dataclasses import dataclass
+from typing import List
+from uuid import UUID
+
+from src.shared_kernel.domain.repositories import IOrganizationalUnitRepository
+
+
+@dataclass(frozen=True)
+class GetOrganizationalUnitByParentIdInputDTO:
+    """
+    Data Transfer Object for get by id requests.
+    """
+
+    parent_id: UUID
+
+
+@dataclass(frozen=True)
+class GetOrganizationalUnitByParentIdOutputDTO:
+    """
+    Data Transfer Object for get by id requests.
+    """
+
+    id: UUID
+    description: str
+    code: str
+    is_active: bool
+
+
+class GetOrganizationalUnitByParentIdUseCase:
+    """
+    Use case for getting an organizational unit by its parent ID.
+    """
+
+    def __init__(self, repository: IOrganizationalUnitRepository) -> None:
+        """
+        Initialize the get by id use case.
+        """
+
+        self._repository = repository
+
+    def execute(
+        self,
+        request: GetOrganizationalUnitByParentIdInputDTO,
+    ) -> List[GetOrganizationalUnitByParentIdOutputDTO]:
+        """
+        Execute the get by parent id use case.
+        """
+
+        entities = self._repository.get_by_parent_id(request.parent_id)
+        return (
+            [
+                GetOrganizationalUnitByParentIdOutputDTO(
+                    id=entity.id,
+                    description=entity.description,
+                    code=entity.code,
+                    is_active=entity.is_active,
+                )
+                for entity in entities
+            ]
+            if entities
+            else []
+        )
