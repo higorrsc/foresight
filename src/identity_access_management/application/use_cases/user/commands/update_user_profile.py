2c904f6e13f1d4fc6063be6854da9ffb1b02e727
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-11-10 21:00:19 -0300

    refactor: ♻ add actor to update user profile use case and correct test

--- src/identity_access_management/application/use_cases/user/commands/update_user_profile.py
+++ src/identity_access_management/application/use_cases/user/commands/update_user_profile.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
 from uuid import UUID
 
 from src.identity_access_management.application.use_cases.user import (
+    InsufficientPermissionError,
     InvalidUserError,
     UserNotFoundError,
 )
+from src.identity_access_management.domain.constants import AppPermission
 from src.identity_access_management.infrastructure.repositories import UserRepository
 from src.shared_kernel.domain._shared import EntityValidationError
+
+if TYPE_CHECKING:
+    from src.identity_access_management.domain.entities import User
 
 
 @dataclass(frozen=True)
@@ -16,7 +21,8 @@ class UserProfileRequestDTO:
     Data Transfer Object for input data when getting a user's profile.
     """
 
-    user_id: UUID
+    actor: "User"
+    user_id_to_update: UUID
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     email: Optional[str] = None
@@ -43,25 +49,55 @@ class UpdateUserProfileUseCase:
         Execute the UpdateUserProfileUseCase.
         """
 
-        user = self._repository.get_by_id(input_dto.user_id)  # type: ignore
-        if not user:
-            raise UserNotFoundError(f"User with id '{input_dto.user_id}' not found.")
+        is_self_update = input_dto.actor.id == input_dto.user_id_to_update
+        can_update_others = AppPermission.USER_UPDATE in input_dto.actor.permissions
+
+        if not is_self_update and not can_update_others:
+            raise InsufficientPermissionError(
+                "User does not have permission to update another user's profile."
+            )
+
+        user_to_update = self._repository.get_by_id(
+            entity_id=input_dto.user_id_to_update,
+            tenant_id=input_dto.actor.tenant_id,
+        )  # type: ignore
+        if not user_to_update:
+            raise UserNotFoundError(
+                f"User with id '{input_dto.user_id_to_update}' not found."
+            )
+
+        something_changed = False
 
         if input_dto.first_name is not None:
-            user.first_name = input_dto.first_name
+            user_to_update.first_name = input_dto.first_name
+            something_changed = True
 
         if input_dto.last_name is not None:
-            user.last_name = input_dto.last_name
+            user_to_update.last_name = input_dto.last_name
+            something_changed = True
 
         if input_dto.email is not None:
-            user.email = input_dto.email
+            user_to_update.email = input_dto.email
+            something_changed = True
 
         if input_dto.is_active is not None:
-            user.is_active = input_dto.is_active
+            if (
+                not can_update_others
+                and input_dto.is_active != user_to_update.is_active
+            ):
+                raise InsufficientPermissionError(
+                    "User does not have permission to change 'is_active' status."
+                )
+            user_to_update.is_active = input_dto.is_active
+            something_changed = True
+
+        if not something_changed:
+            return
 
         try:
-            user.validate()
+            user_to_update.updated_by = input_dto.actor.id
+            user_to_update.validate()
         except EntityValidationError as e:
             raise InvalidUserError(f"Invalid user data: {e}") from e
 
-        self._repository.update(user)
+        self._repository.update(user_to_update)
--- tests/identity_access_management/application/use_cases/user/commands/test_update_user_profile.py
+++ tests/identity_access_management/application/use_cases/user/commands/test_update_user_profile.py
@@ -1,111 +1,162 @@
-from uuid import uuid4
+from copy import deepcopy
 
 import pytest
 
 from src.identity_access_management.application.use_cases.user import (
-    InvalidUserError,
-    UserNotFoundError,
+    InsufficientPermissionError,
 )
 from src.identity_access_management.application.use_cases.user.commands import (
     UpdateUserProfileUseCase,
     UserProfileRequestDTO,
 )
-from src.identity_access_management.domain.entities import User
-from src.identity_access_management.domain.entities.user import hash_password
+from src.identity_access_management.domain.constants import AppPermission
+from src.identity_access_management.domain.entities.user import User
 from tests.fakes.in_memory_repository import UserInMemoryRepository
 
 
-class TestUpdateUserProfile:
+@pytest.fixture
+def user_repo():
     """
-    Test suite for the UpdateUserProfileUseCase.
+    Fixture that represents a user repository.
     """
 
-    @pytest.fixture
-    def user_in_memory_repository(self):
-        """
-        Fixture that represents an in-memory repository for testing purposes.
-        """
+    return UserInMemoryRepository()
 
-        return UserInMemoryRepository()
 
-    def test_update_user_profile_successfully(self, user_in_memory_repository):
-        """
-        Test update user profile successfully.
-        """
+@pytest.fixture
+def update_user_profile_use_case(user_repo):
+    """
+    Fixture that represents an UpdateUserProfileUseCase.
+    """
 
-        user = User(
-            username="testuser",
-            hashed_password=hash_password("foresight"),
-        )
-        user_in_memory_repository.save(user)
+    return UpdateUserProfileUseCase(user_repo)
 
-        input_dto = UserProfileRequestDTO(
-            user_id=user.id,
-            first_name="John",
-            last_name="Doe",
-            email="john.doe@email.com",
-            is_active=True,
-        )
 
-        use_case = UpdateUserProfileUseCase(repository=user_in_memory_repository)
-        use_case.execute(input_dto)
+class TestUpdateUserProfileUseCase:
 
-        output = user_in_memory_repository.get_by_id(user.id)
-
-        assert output is not None
-        assert output.username == user.username
-        assert output.first_name == "John"
-        assert output.last_name == "Doe"
-        assert output.email == "john.doe@email.com"
-        assert output.is_active is True
-
-    def test_update_user_profile_with_invalid_email_raises_error(
+    def test_user_can_update_own_profile(
         self,
-        user_in_memory_repository,
+        update_user_profile_use_case,
+        user_repo,
+        guest_actor: User,
     ):
         """
-        Test update user profile with invalid email.
+        Test if a user can update their own profile.
         """
 
-        user = User(
-            username="testuser",
-            hashed_password=hash_password("foresight"),
-        )
-        user_in_memory_repository.save(user)
+        user_repo.save(deepcopy(guest_actor))
 
         input_dto = UserProfileRequestDTO(
-            user_id=user.id,
-            first_name="John",
-            last_name="Doe",
-            email="john.doe#email.com",
-            is_active=True,
+            actor=guest_actor,
+            user_id_to_update=guest_actor.id,
+            first_name="Guesty",
         )
-        use_case = UpdateUserProfileUseCase(user_in_memory_repository)
 
-        with pytest.raises(
-            InvalidUserError,
-            match="Invalid user data: An email address must have an @-sign.",
-        ):
-            use_case.execute(input_dto)
+        update_user_profile_use_case.execute(input_dto)
 
-    def test_update_user_profile_with_invalid_id(self, user_in_memory_repository):
+        updated_user = user_repo.get_by_id(guest_actor.id, guest_actor.tenant_id)
+        assert updated_user.first_name == "Guesty"
+        assert updated_user.updated_by == guest_actor.id
+
+    def test_admin_can_update_other_user_profile(
+        self,
+        update_user_profile_use_case,
+        user_repo,
+        admin_actor: User,
+        guest_actor: User,
+    ):
         """
-        Test update user with invalid id.
+        Test if an admin can update the profile of another user.
         """
 
-        invalid_id = uuid4()
+        admin_actor.permissions.add(AppPermission.USER_UPDATE)
+
+        user_repo.save(deepcopy(admin_actor))
+        user_repo.save(deepcopy(guest_actor))
 
         input_dto = UserProfileRequestDTO(
-            user_id=invalid_id,
-            first_name="John",
-            last_name="Doe",
-            email="john.doe#email.com",
-            is_active=True,
+            actor=admin_actor,
+            user_id_to_update=guest_actor.id,
+            last_name="McGuest",
         )
-        use_case = UpdateUserProfileUseCase(user_in_memory_repository)
+
+        update_user_profile_use_case.execute(input_dto)
+
+        updated_guest = user_repo.get_by_id(guest_actor.id, admin_actor.tenant_id)
+        assert updated_guest.last_name == "McGuest"
+        assert updated_guest.updated_by == admin_actor.id
+
+    def test_user_cannot_update_other_user_profile(
+        self,
+        update_user_profile_use_case,
+        user_repo,
+        guest_actor: User,
+        admin_actor: User,
+    ):
+        """
+        Test if a user cannot update the profile of another user.
+        """
+
+        user_repo.save(deepcopy(admin_actor))
+        user_repo.save(deepcopy(guest_actor))
+
+        input_dto = UserProfileRequestDTO(
+            actor=guest_actor,
+            user_id_to_update=admin_actor.id,
+            first_name="Hacker",
+        )
 
         with pytest.raises(
-            UserNotFoundError,
-            match=f"User with id '{invalid_id}' not found.",
+            InsufficientPermissionError,
+            match="User does not have permission to update another user's profile.",
         ):
-            use_case.execute(input_dto)
+            update_user_profile_use_case.execute(input_dto)
+
+    def test_user_cannot_change_is_active_status_of_self(
+        self,
+        update_user_profile_use_case,
+        user_repo,
+        guest_actor: User,
+    ):
+        """
+        Test if a user cannot change its own 'is_active' status.
+        """
+
+        user_repo.save(deepcopy(guest_actor))
+
+        input_dto = UserProfileRequestDTO(
+            actor=guest_actor,
+            user_id_to_update=guest_actor.id,
+            is_active=False,
+        )
+
+        with pytest.raises(
+            InsufficientPermissionError,
+            match="User does not have permission to change 'is_active' status.",
+        ):
+            update_user_profile_use_case.execute(input_dto)
+
+    def test_admin_can_change_is_active_status(
+        self,
+        update_user_profile_use_case,
+        user_repo,
+        admin_actor: User,
+        guest_actor: User,
+    ):
+        """
+        Test if an admin can change the 'is_active' status of a user.
+        """
+
+        admin_actor.permissions.add(AppPermission.USER_UPDATE)
+        user_repo.save(deepcopy(admin_actor))
+        user_repo.save(deepcopy(guest_actor))
+
+        input_dto = UserProfileRequestDTO(
+            actor=admin_actor,
+            user_id_to_update=guest_actor.id,
+            is_active=False,
+        )
+
+        update_user_profile_use_case.execute(input_dto)
+        updated_guest = user_repo.get_by_id(guest_actor.id, admin_actor.tenant_id)
+        assert updated_guest.is_active is False
