23432982a671c63856684c267d961ffd9761bbfe
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-11-11 20:43:15 -0300

    feat: :sparkles: get permission, tenant, plan and organization unit repositories database dependencies

--- src/api/dependencies/database.py
+++ src/api/dependencies/database.py
@@ -4,11 +4,19 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from src.identity_access_management.infrastructure.repositories import (
+    PermissionRepository,
     RoleRepository,
     UserRepository,
 )
 from src.shared_kernel.infrastructure.config import SessionLocal
-from src.shared_kernel.infrastructure.repositories import AreaRepository
+from src.shared_kernel.infrastructure.repositories import (
+    AreaRepository,
+    OrganizationalUnitRepository,
+)
+from src.tenant_management.infrastructure.repositories import (
+    PlanRepository,
+    TenantRepository,
+)
 
 
 def get_db_session() -> Generator:  # pragma: no cover
@@ -54,3 +62,43 @@ def get_role_repository(
     """
 
     return RoleRepository(session)
+
+
+def get_permission_repository(
+    session: Session = Depends(get_db_session),
+) -> PermissionRepository:
+    """
+    Return an PermissionRepository instance with database session.
+    """
+
+    return PermissionRepository(session)
+
+
+def get_tenant_repository(
+    session: Session = Depends(get_db_session),
+) -> TenantRepository:
+    """
+    Return an TenantRepository instance with database session.
+    """
+
+    return TenantRepository(session)
+
+
+def get_plan_repository(
+    session: Session = Depends(get_db_session),
+) -> PlanRepository:
+    """
+    Return an PlanRepository instance with database session.
+    """
+
+    return PlanRepository(session)
+
+
+def get_organization_unit_repository(
+    session: Session = Depends(get_db_session),
+) -> OrganizationalUnitRepository:
+    """
+    Return an OrganizationalUnitRepository instance with database session.
+    """
+
+    return OrganizationalUnitRepository(session)
