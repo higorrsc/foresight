b8560b6bb37b9977d372953ebbe60fa0f6b63064
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-11-12 22:39:04 -0300

    refactor: :recycle: use generic list use case at list user

--- src/identity_access_management/application/use_cases/user/queries/list_user.py
+++ src/identity_access_management/application/use_cases/user/queries/list_user.py
@@ -1,51 +1,18 @@
-from src.identity_access_management.application.use_cases.user import (
-    InsufficientPermissionError,
-)
-from src.identity_access_management.domain.constants import AppPermission
 from src.identity_access_management.domain.entities import User
-from src.identity_access_management.domain.repositories import IUserRepository
-from src.shared_kernel.application._shared.use_cases.queries import ListRequestInputDTO
-from src.shared_kernel.domain._shared.repository import PaginatedResult
+from src.shared_kernel.application._shared.use_cases.queries import GenericListUseCase
+from src.shared_kernel.domain._shared.repository import AbstractRepository
 
 
-class ListUserUseCase:
+class ListUserUseCase(GenericListUseCase[User]):
     """
     Use case to list users.
     """
 
-    def __init__(self, repository: IUserRepository) -> None:
+    def __init__(self, repository: AbstractRepository[User]) -> None:
         """
         Initialize the ListUserUseCase.
 
         :param repository: The repository to use for listing users.
         """
 
-        self._repository = repository
-
-    def execute(self, input_dto: ListRequestInputDTO) -> PaginatedResult[User]:
-        """
-        Execute the ListUserUseCase.
-
-        :param input_dto: The input data transfer object.
-        :return: A list of users.
-        """
-
-        if AppPermission.USER_READ not in input_dto.actor.permissions:
-            raise InsufficientPermissionError(
-                "User does not have permission to list users."
-            )
-
-        paginated_result = self._repository.search(
-            tenant_id=input_dto.actor.tenant_id,
-            filters=input_dto.filters,
-            sort_by=input_dto.sort_by,
-            sort_order=input_dto.sort_order,  # type: ignore
-            offset=input_dto.offset,
-            limit=input_dto.limit,
-            include_inactive=input_dto.include_inactive,
-        )
-
-        return PaginatedResult(
-            data=paginated_result.data,
-            total=paginated_result.total,
-        )
+        super().__init__(repository)
