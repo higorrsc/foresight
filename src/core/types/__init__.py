1b0291679741b4b04b765873e65002b0ce64fe2e
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-04-30 19:51:57 -0300

    feat: :sparkles: add TypeGuard utilities for runtime type narrowing (soft delete, auditing and tenant)

---
+++ src/core/types/__init__.py
@@ -0,0 +1,17 @@
+from .guards import (
+    HasTenant,
+    SoftDeletable,
+    UserAuditable,
+    has_tenant,
+    is_soft_deletable,
+    is_user_auditable,
+)
+
+__all__ = [
+    "HasTenant",
+    "SoftDeletable",
+    "UserAuditable",
+    "has_tenant",
+    "is_soft_deletable",
+    "is_user_auditable",
+]
