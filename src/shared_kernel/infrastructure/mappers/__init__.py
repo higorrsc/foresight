20d9ea8b74a928a79632111d7fcac11c7342615f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-05-01 15:54:30 -0300

    feat: :sparkles: add financial_scenario mapper

--- src/shared_kernel/infrastructure/mappers/__init__.py
+++ src/shared_kernel/infrastructure/mappers/__init__.py
@@ -1,7 +1,9 @@
 from .area_mapper import AreaMapper
+from .financial_scenario import FinancialScenarioMapper
 from .organizational_unit_mapper import OrganizationalUnitMapper
 
 __all__ = [
     "AreaMapper",
     "OrganizationalUnitMapper",
+    "FinancialScenarioMapper",
 ]
---
+++ src/shared_kernel/infrastructure/mappers/financial_scenario.py
@@ -0,0 +1,54 @@
+from src.core.infrastructure.mappers.base_mapper import BaseMapper
+from src.core.infrastructure.mappers.mapper import AbstractMapper
+from src.shared_kernel.domain.entities import FinancialScenario
+from src.shared_kernel.infrastructure.models import FinancialScenarioModel
+
+
+class FinancialScenarioMapper(
+    AbstractMapper[FinancialScenario, FinancialScenarioModel]
+):
+    """
+    Mapper class to convert between FinancialScenario entity and FinancialScenarioModel.
+    """
+
+    @staticmethod
+    def to_model(entity: FinancialScenario) -> FinancialScenarioModel:
+        """
+        Converts a FinancialScenario entity to a FinancialScenarioModel instance.
+        """
+
+        model = FinancialScenarioModel(
+            id=entity.id,
+            tenant_id=entity.tenant_id,
+            description=entity.description,
+            scenario_type=entity.scenario_type,
+            is_locked=entity.is_locked,
+            assumptions=entity.assumptions,
+            is_active=entity.is_active,
+            created_at=entity.created_at,
+            updated_at=entity.updated_at,
+        )
+
+        BaseMapper.map_auditing_fields_to_model(entity, model)
+        return model
+
+    @staticmethod
+    def to_entity(model: FinancialScenarioModel) -> FinancialScenario:
+        """
+        Converts a FinancialScenarioModel instance to a FinancialScenario entity.
+        """
+
+        entity = FinancialScenario(
+            id=model.id,  # type: ignore
+            tenant_id=model.tenant_id,  # type: ignore
+            description=model.description,  # type: ignore
+            scenario_type=model.scenario_type,  # type: ignore
+            is_locked=model.is_locked,  # type: ignore
+            assumptions=model.assumptions,  # type: ignore
+            is_active=model.is_active,  # type: ignore
+            created_at=model.created_at,  # type: ignore
+            updated_at=model.updated_at,  # type: ignore
+        )
+
+        BaseMapper.map_auditing_fields_to_entity(model, entity)
+        return entity
