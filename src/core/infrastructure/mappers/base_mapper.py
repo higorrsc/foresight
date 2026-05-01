0e4edac1400902dc46ddaf127df6ae5048f18952
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-05-01 15:56:05 -0300

    feat: :sparkles: add common fields to base mapper

--- src/core/infrastructure/mappers/base_mapper.py
+++ src/core/infrastructure/mappers/base_mapper.py
@@ -6,31 +6,31 @@ class BaseMapper:
     Provides common mapping functionalities for auditing and soft-delete fields.
     """
 
-    @staticmethod
-    def map_auditing_fields_to_model(entity: Any, model: Any) -> None:
+    COMMON_FIELDS_TO_MAP = [
+        "created_by",
+        "created_at",
+        "updated_by",
+        "updated_at",
+        "is_active",
+        "deleted_at",
+    ]
+
+    @classmethod
+    def map_auditing_fields_to_model(cls, entity: Any, model: Any) -> None:
         """
         Maps auditing and soft-delete fields from an entity to a model.
-
-        This method checks for the existence of 'created_by', 'updated_by',
-        and 'deleted_at' on the entity and assigns them to the model if present.
         """
-        if hasattr(entity, "created_by"):
-            model.created_by = entity.created_by
 
-        if hasattr(entity, "updated_by"):
-            model.updated_by = entity.updated_by
+        for field in cls.COMMON_FIELDS_TO_MAP:
+            if hasattr(entity, field):
+                setattr(model, field, getattr(entity, field))
 
-        if hasattr(entity, "deleted_at"):
-            model.deleted_at = entity.deleted_at
-
-    @staticmethod
-    def map_auditing_fields_to_entity(model: Any, entity: Any) -> None:
+    @classmethod
+    def map_auditing_fields_to_entity(cls, model: Any, entity: Any) -> None:
         """
         Maps auditing and soft-delete fields from a model to an entity.
-
-        This method uses setattr to safely assign 'created_by', 'updated_by',
-        and 'deleted_at' from the model to the entity if the attributes exist on both.
         """
-        for attr in ["created_by", "updated_by", "deleted_at"]:
-            if hasattr(model, attr) and hasattr(entity, attr):
-                setattr(entity, attr, getattr(model, attr))
+
+        for field in cls.COMMON_FIELDS_TO_MAP:
+            if hasattr(model, field) and hasattr(entity, field):
+                setattr(entity, field, getattr(model, field))
