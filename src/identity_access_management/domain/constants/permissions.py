6fed833ae57cd6ed8dfd5b3257f8d52ea043210f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-05-08 19:29:08 -0300

    refactor: :recycle: rename scenario permission by removing "financial_" prefix

--- src/identity_access_management/domain/constants/permissions.py
+++ src/identity_access_management/domain/constants/permissions.py
@@ -16,10 +16,10 @@ class AppPermission(StrEnum):
     ORGANIZATIONAL_UNIT_READ = "organizational_unit:read"
     ORGANIZATIONAL_UNIT_UPDATE = "organizational_unit:update"
 
-    FINANCIAL_SCENARIO_CREATE = "financial_scenario:create"
-    FINANCIAL_SCENARIO_DELETE = "financial_scenario:delete"
-    FINANCIAL_SCENARIO_READ = "financial_scenario:read"
-    FINANCIAL_SCENARIO_UPDATE = "financial_scenario:update"
+    SCENARIO_CREATE = "scenario:create"
+    SCENARIO_DELETE = "scenario:delete"
+    SCENARIO_READ = "scenario:read"
+    SCENARIO_UPDATE = "scenario:update"
 
     PLAN_CREATE = "plan:create"
     PLAN_DELETE = "plan:delete"
