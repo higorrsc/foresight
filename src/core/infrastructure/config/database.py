cf6fd1fb886c65d8ac16756250f4620c798c5f2e
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-05-08 21:09:45 -0300

    refactor: :recycle: change database session to async

--- src/core/infrastructure/config/database.py
+++ src/core/infrastructure/config/database.py
@@ -1,14 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from .settings import settings
 
 SQLALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)  # type: ignore
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)  # type: ignore
 
-SessionLocal = sessionmaker(
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
     autocommit=False,
     autoflush=False,
-    bind=engine,
 )
